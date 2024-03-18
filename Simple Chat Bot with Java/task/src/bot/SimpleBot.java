fa89e6a343d624567088dcb960a6226b00fc120f
Work <yakushevso@ya.ru>
2024-03-18 12:04:40 +0300

    Stage 4/5: Teach your bot to count

--- Simple Chat Bot with Java/task/src/bot/SimpleBot.java
+++ Simple Chat Bot with Java/task/src/bot/SimpleBot.java
@@ -19,8 +19,21 @@ public class SimpleBot {
                 Enter remainders of dividing your age by 3, 5 and 7.
                 """, name);
 
-        int age = (scanner.nextInt() * 70 + scanner.nextInt() * 21 + scanner.nextInt() * 15) % 105;
+        int rem3 = scanner.nextInt();
+        int rem5 = scanner.nextInt();
+        int rem7 = scanner.nextInt();
 
-        System.out.printf("Your age is %d; that's a good time to start programming!", age);
+        int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
+
+        System.out.println("Your age is " + age + "; that's a good time to start programming!");
+        System.out.println("Now I will prove to you that I can count to any number you want.");
+
+        int num = scanner.nextInt();
+
+        for (int i = 0; i <= num; i++) {
+            System.out.println(i + "!");
+        }
+
+        System.out.println("Completed, have a nice day!");
     }
 }
