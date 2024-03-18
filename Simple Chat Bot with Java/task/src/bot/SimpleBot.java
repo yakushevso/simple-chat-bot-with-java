64b551d969e0b6943b9fa2b32e488c6763a3dbcf
Work <yakushevso@ya.ru>
2024-03-18 11:55:45 +0300

    Stage 2/5: Introduce yourself

--- Simple Chat Bot with Java/task/src/bot/SimpleBot.java
+++ Simple Chat Bot with Java/task/src/bot/SimpleBot.java
@@ -1,10 +1,16 @@
 package bot;
 
+import java.util.Scanner;
+
 public class SimpleBot {
     public static void main(String[] args) {
-        // write your code here
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("""
-                        Hello! My name is Aid.
-                        I was created in 2020.""");
+                Hello! My name is Aid.");
+                I was created in 2018
+                Please, remind me your name.""");
+
+        System.out.printf("What a great name you have, %s!", scanner.next());
     }
 }
