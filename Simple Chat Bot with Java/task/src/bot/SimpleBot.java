6f74ea0b21457da65f0d8c1915ad832a5372ac05
Work <yakushevso@ya.ru>
2024-03-18 12:01:15 +0300

    Stage 3/5: Let the chatbot guess your age

--- Simple Chat Bot with Java/task/src/bot/SimpleBot.java
+++ Simple Chat Bot with Java/task/src/bot/SimpleBot.java
@@ -8,9 +8,19 @@ public class SimpleBot {
 
         System.out.println("""
                 Hello! My name is Aid.");
-                I was created in 2018
+                I was created in 2018.
                 Please, remind me your name.""");
 
-        System.out.printf("What a great name you have, %s!", scanner.next());
+        String name = scanner.nextLine();
+
+        System.out.printf("""
+                What a great name you have, %s!
+                Let me guess your age.
+                Enter remainders of dividing your age by 3, 5 and 7.
+                """, name);
+
+        int age = (scanner.nextInt() * 70 + scanner.nextInt() * 21 + scanner.nextInt() * 15) % 105;
+
+        System.out.printf("Your age is %d; that's a good time to start programming!", age);
     }
 }
